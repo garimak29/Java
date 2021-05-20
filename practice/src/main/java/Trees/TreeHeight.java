38487325fa8265fb83125cfb2d7aed375f96e3f7
garimak29 <garimak29@gmail.com>
2021-05-20 17:27:45 +0530

    m

--- practice/src/main/java/Trees/TreeHeight.java
+++ practice/src/main/java/Trees/TreeHeight.java
@@ -1,5 +1,43 @@
-package com.example.practice;
+package Trees;
+
+import java.util.Stack;
 
 public class TreeHeight {
+	
+	 
+	  class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) {
+	       val = x;
+	       left=null;
+	       right=null;
+	      }
+	  }
+	 
+	public class Solution {
+	    public int solve(TreeNode A) {
+	    	int res =0;
+	    	Stack stack = new Stack();
+	    	TreeNode curr = A;
+	    	System.out.println(curr);
+	    	while(curr != null || !stack.isEmpty()) {
+	    		if(curr !=null) {
+	    			res ++;
+	    			stack.push(curr);
+	    			curr = curr.left;
+	    		}
+	    		else
+	    		{
+	    			TreeNode temp = (TreeNode) stack.peek();
+	    			stack.pop();
+	    			curr = temp.right;
+	    		}
+	    	}
+	    	return res ;
+	    }
+	}
+
 
 }
